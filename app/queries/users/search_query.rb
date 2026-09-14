aa7c0d22d432a7e21cf695f27ba2b58b5f939d6a
brusatimatias <mformento8@gmail.com>
2026-09-13 21:01:43 -0300

    Refactor in Paginatable module

--- app/queries/users/search_query.rb
+++ app/queries/users/search_query.rb
@@ -1,34 +1,23 @@
 module Users
   class SearchQuery
-    DEFAULT_PER_PAGE = 20
-    MAX_PER_PAGE = 50
-    INVALID_PAGINATION_MESSAGE =
-      "Pagination values must be positive integers and per_page cannot exceed #{MAX_PER_PAGE}"
+    include Paginatable
 
-    class InvalidPagination < StandardError; end
-
-    def initialize(current_user, query: nil, page: 1, per_page: DEFAULT_PER_PAGE)
+    def initialize(current_user, query: nil, page: 1, per_page: Paginatable::DEFAULT_PER_PAGE)
       @current_user = current_user
       @query = query.to_s.strip
-      @page = parse_positive_integer(page)
-      @per_page = parse_per_page(per_page)
+      paginate(page: page, per_page: per_page)
     end
 
     def call
       {
         users: paginated_users,
-        meta: {
-          current_page: page,
-          per_page: per_page,
-          total_pages: total_pages,
-          total_count: total_count
-        }
+        meta: pagination_meta(total_count)
       }
     end
 
     private
 
-    attr_reader :current_user, :query, :page, :per_page
+    attr_reader :current_user, :query
 
     def users
       scope = User.where.not(id: current_user.id)
@@ -48,28 +37,11 @@ module Users
     end
 
     def paginated_users
-      users.offset((page - 1) * per_page).limit(per_page)
+      users.offset(pagination_offset).limit(per_page)
     end
 
     def total_count
       @total_count ||= users.unscope(:order).count
-    end
-
-    def total_pages
-      (total_count.to_f / per_page).ceil
-    end
-
-    def parse_positive_integer(value)
-      return value.to_i if value.to_s.match?(/\A[1-9]\d*\z/)
-
-      raise InvalidPagination, INVALID_PAGINATION_MESSAGE
-    end
-
-    def parse_per_page(value)
-      parsed_value = parse_positive_integer(value)
-      return parsed_value if parsed_value <= MAX_PER_PAGE
-
-      raise InvalidPagination, INVALID_PAGINATION_MESSAGE
     end
   end
 end
